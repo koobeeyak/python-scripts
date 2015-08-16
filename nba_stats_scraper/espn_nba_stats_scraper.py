62473533f6bcc2b3d645fc70341ef8f314f8ab38
kubiak <phkubiak@gmail.com>
2015-08-16 15:03:24 -0400

    First soup function was redundant. Prompting user with names of teams, returning link

--- nba_stats_scraper/espn_nba_stats_scraper.py
+++ nba_stats_scraper/espn_nba_stats_scraper.py
@@ -6,14 +6,10 @@ import re, time
 
 BASE_URL = "http://espn.go.com/nba/"
 
-def scrape_nba_teams():
+def generate_team_abbrev_team_name_dict():
 	url = BASE_URL + "teams"  # REMEMBER that the url for a specific team will be http://espn.go.com/nba/team/_/name/
 	response = requests.get(url)
 	soup = BeautifulSoup(response.text)
-	return soup
-
-def generate_team_abbrev_team_name_dict():
-	soup = scrape_nba_teams()
 	team_dict = {}
 	for team_link in soup.find_all('a', href = re.compile(BASE_URL + "team/_/name/*")):
 		print team_link
@@ -27,18 +23,22 @@ def generate_team_abbrev_team_name_dict():
 
 def prompt_user_for_team():
 	d = generate_team_abbrev_team_name_dict()
-	print "Enter the abbreviation for the team you would like to see stats for."
+	# TODO: can I make this more user friendly by isolating team name abbreviation from link using regex
+	# and having user type abbreviation instead of full name?
+	# 	find_abbrev = re.match(r"^.*\/name\/(.*)\/.*$",team_link['href'])
+	#	team_abbrev = find_abbrev.group(1)
+	print "Which team would you like to see?"
 	time.sleep(2)
 	for p,v in d.iteritems():
-		print "Enter %s for %s." % (p,v)
+		print "%s" % (p) 
 	chosen_team = raw_input("> ")
 	while True:
 		if chosen_team in d.keys():
 			break
 		else:
-			print "Enter one of the abbreviations above."
-			chosen_team = raw_input("> ").lower()
-	return d[chosen_team]
+			print "Please enter the exact name of a team above." # TODO: fix this by autocapitalizing first letters and removing whitespace
+			chosen_team = raw_input("> ")
+	return d[chosen_team] # this is the link to the team's page
 
 if __name__ == "__main__":
 	print prompt_user_for_team()
